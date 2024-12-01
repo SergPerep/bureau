34f045cef7588a7bccf9ad33df7dca9935db235a
sergperep <perepelkin.serge@gmail.com>
2024-12-01 20:28:11 +0100

    Feat: initialize project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<form action="" method="post">
+    <input type="file" accept=".txt"/>
+    <input type="submit" value="Send"/>
+</form>
